10f34ca645c5bcc94a0c9ebddd3db36c928df956
Amr Mohamed Shawky <amrshawky936@gmail.com>
2025-12-11 17:37:10 +0200

    Add using directives for category update features
    Added references to UpdateCategory and UpdateCategoryStatus features in Program.cs to enable category update and status management functionality.

--- Catalog Service/Program.cs
+++ Catalog Service/Program.cs
@@ -2,6 +2,8 @@ using BuildingBlocks.Interfaces;
 using BuildingBlocks.MiddleWares; // Ensure you have this namespace/folder
 using BuildingBlocks.SharedEntities;
 using Catalog_Service.Entities;
+using Catalog_Service.Features.CategoriesFeature.UpdateCategory;
+using Catalog_Service.Features.CategoriesFeature.UpdateCategoryStatus;
 using Catalog_Service.Features.OccasionsFeature.CreateOccasion;
 using Catalog_Service.Infrastructure;
 using Catalog_Service.Infrastructure.Data;
